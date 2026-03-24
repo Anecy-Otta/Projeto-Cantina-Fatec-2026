34bc217ef1fe3f72aaa34e06bc8173b1d703b607
Anecy-Otta <anecy.otta@icloud.com>
2026-03-23 21:45:22 -0300

    Implementei estruturas
    Implementei estruturas:
    - FilaEstoque: controle de entrada e saída de produtos
    - ListaPagamentos: armazenamento de pagamentos
    - ListaConsumo: registro de consumos vinculados a produtos e pagamentos

--- estrutura.py
+++ estrutura.py
@@ -1,1 +1,27 @@
+class FilaEstoque:
+    def __init__(self):
+        self.itens = []
 
+    def enqueue(self, produto):
+        self.itens.append(produto)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.itens.pop(0)
+
+    def is_empty(self):
+        return len(self.itens) == 0
+    
+class ListaPagamentos:
+    def __init__(self):
+        self.pagamentos = []
+
+    def adicionar(self, pagamento):
+        self.pagamentos.append(pagamento)
+
+class ListaConsumo:
+    def __init__(self):
+        self.consumos = []
+
+    def adicionar(self, consumo):
+        self.consumos.append(consumo)
