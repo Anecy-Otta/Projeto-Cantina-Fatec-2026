a44049e0a1d966f6b981d5618b0fe8b843a25c72
Anecy-Otta <anecy.otta@icloud.com>
2026-03-26 08:16:51 -0300

    Gravar pasta data

--- utils.py
+++ utils.py
@@ -1,37 +1,45 @@
 import pickle
+import os 
 import random
 from faker import Faker
-from models import Produto, Pagamento
+from models import Produto
 
 fake = Faker('pt_BR')
 
-def salvar(dados, arquivo):
-    """Salva qualquer objeto em um arquivo binário."""
-    with open(arquivo, 'wb') as f:
-        pickle.dump(dados, f)
+def salvar(dados, caminho_arquivo):
+    pasta = os.path.dirname(caminho_arquivo)
+    
+    if pasta and not os.path.exists(pasta):
+        os.makedirs(pasta)
+        print(f">> Pasta '{pasta}' criada com sucesso.")
+
+    try:
+        with open(caminho_arquivo, 'wb') as f:
+            pickle.dump(dados, f)
+        print(f">> Arquivo '{caminho_arquivo}' gravado com sucesso!")
+    except Exception as e:
+        print(f">> Erro ao gravar arquivo: {e}")
 
 def carregar(arquivo):
-    """Carrega dados de um arquivo binário. Retorna None se o arquivo não existir."""
+    if not os.path.exists(arquivo):
+        return None
     try:
         with open(arquivo, 'rb') as f:
             return pickle.load(f)
-    except FileNotFoundError:
+    except Exception as e:
+        print(f">> Erro ao carregar: {e}")
         return None
 
-def gerar_massa_dados(qtd_produtos=5):
-    """Gera uma lista de produtos fakes para popular o sistema."""
+def gerar_massa_dados(qtd=5):
     produtos_fakes = []
-    for _ in range(qtd_produtos):
-        p_compra = round(random.uniform(1.0, 5.0), 2)
-        p_venda = round(p_compra * 1.8, 2)
-        
+    for _ in range(qtd):
         p = Produto(
-            nome=fake.bothify(text='Lanche ??-##'),
-            preco_compra=p_compra,
-            preco_venda=p_venda,
+            nome=fake.word().capitalize(),
+            preco_compra=round(random.uniform(2.0, 5.0), 2),
+            preco_venda=round(random.uniform(6.0, 12.0), 2),
             data_compra="2026-03-26",
             data_validade="2026-12-31",
-            quantidade=random.randint(10, 100)
+            quantidade=random.randint(10, 50)
         )
         produtos_fakes.append(p)
     return produtos_fakes
